bfae5766b1944ae864f584694583ef175ba9eda3
viacrusis_chris - RD&P Committee <113983506+paranoid-android12@users.noreply.github.com>
2023-07-03 17:09:00 +0800

    part 1.1

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,18 +1,45 @@
-const Hello = (props) => {
-  return (
+const Header = (course) => {
+  return(
     <div>
-
-      <p>Hello {props.name}</p>
+      <h1>{course.name}</h1>
     </div>
   )
 }
 
+const Content = (exercise) => {
+  return(
+    <div>
+      <p>{exercise.name} {exercise.count}</p>
+    </div>
+  )
+}
+
+const Total = (total) => {
+  return(
+    <div>
+      <p>Number of exercises {total.num1 + total.num2 + total.num3}</p>
+    </div>
+  )
+}
+
+
+
 const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+  console.log(course)
   return (
     <div>
-      <h1>Greetings</h1>
-      <Hello name='Chris' />
-      <Hello />
+      <Header name={course} />
+      <Content name={part1} count={exercises1} />
+      <Content name={part2} count={exercises2} />
+      <Content name={part3} count={exercises3} />
+      <Total num1={exercises1} num2={exercises2} num3={exercises3} />
     </div>
   )
 }
