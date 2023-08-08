f3d2d205244218cd3b7397bd12d3164a0bb35714
viacrusis_chris - RD&P Committee <113983506+paranoid-android12@users.noreply.github.com>
2023-08-08 11:57:22 +0800

    Updated answers (temp)

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,17 +1,46 @@
+import { useState } from 'react'
+import Note from './components/Note'
+
 const App = (props) => {
-  const { notes } = props
-  console.log(notes.map(x => x.id))
+  const [notes, setNotes] = useState(props.notes)
+
+  const [newNote, setNewNote] = useState(
+    'a new note...'
+  ) 
+
+  
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value)
+  }
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      id: notes.length + 1,
+      content: newNote,
+      important: Math.random() < 0.5,
+    }
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
+  }
+
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-        {notes.map(x => 
-        <li key={x.id}>
-          {x.content}
-        </li>)}
+        {notes.map(note => 
+          <Note key={note.id} note={note} />
+        )}
       </ul>
+      <form onSubmit={addNote}>
+        <input
+          value={newNote}
+          onChange={handleNoteChange}
+        />
+        <button type="submit">save</button>
+      </form>  
     </div>
   )
 }
 
-export default App
+export default App 
